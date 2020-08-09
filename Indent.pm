011ab9324a94b13228c01a8f4d05aebb25e365ce
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2020-08-09 13:33:59 +0200

    Prepare for 0.02 version

--- Indent.pm
+++ Indent.pm
@@ -11,7 +11,7 @@ use Readonly;
 Readonly::Scalar my $EMPTY_STR => q{};
 Readonly::Scalar my $SPACE => q{ };
 
-our $VERSION = 0.01;
+our $VERSION = 0.02;
 
 # Resets internal variables.
 sub reset {
@@ -307,6 +307,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.01
+0.02
 
 =cut
