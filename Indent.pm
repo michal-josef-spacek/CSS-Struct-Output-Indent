d113f90cfbe0f896b6a140674b07a62307d31324
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2021-01-28 11:09:19 +0100

    Add EXAMPLE section to doc

--- Indent.pm
+++ Indent.pm
@@ -265,6 +265,32 @@ CSS::Struct::Output::Indent - Indent printing 'CSS::Struct' structure to CSS cod
          Output handler is bad file handler.
          Unknown parameter '%s'.
 
+=head1 EXAMPLE
+
+ use strict;
+ use warnings;
+
+ use CSS::Struct::Output::Indent;
+
+ my $css = CSS::Struct::Output::Indent->new(
+         'output_handler' => \*STDOUT,
+ );
+
+ $css->put(['s', 'selector#id']);
+ $css->put(['s', 'div div']);
+ $css->put(['s', '.class']);
+ $css->put(['d', 'weight', '100px']);
+ $css->put(['d', 'font-size', '10em']);
+ $css->put(['e']);
+ $css->flush;
+ print "\n";
+
+ # Output:
+ # selector#id, div div, .class {
+ #         weight: 100px;
+ #         font-size: 10em;
+ # }
+
 =head1 DEPENDENCIES
 
 L<CSS::Struct::Output>,
